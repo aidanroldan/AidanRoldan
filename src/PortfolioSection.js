345549e07ed536fcdbfeed9087800c62c84b7f61
skippybonez <108502188+skippybonez@users.noreply.github.com>
2024-01-21 21:57:57 -0500

    humble man trying to flex

--- src/PortfolioSection.js
+++ src/PortfolioSection.js
@@ -10,10 +10,24 @@ function PortfolioSection() {
           <Card>
             <Card.Img variant="top" src="path-to-image" />
             <Card.Body>
-              <Card.Title>Project Title</Card.Title>
+              <Card.Title>LMS</Card.Title>
               <Card.Text>
-                Brief description of the project.
+                Loan Managment System is a tool developed for a private client in order to manage his cash advance business. 
+                Database and site both hosted on AWS Ec2.
+                 Functionality includes, reating loans with different interest criteria, add and modifying payments was a big part of this project as the world of debts can be
               </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col md={4}>
+          <Card>
+            <Card.Img variant="top" src="path-to-image" />
+            <Card.Body>
+              <Card.Title>Arduino Friends</Card.Title>
+              <Card.Text>
+                Arduino friends was a fairly simple project which I had a lot of fun doing.
+                 </Card.Text>
             </Card.Body>
           </Card>
         </Col>
