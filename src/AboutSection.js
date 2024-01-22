156631b3c28c67ccc5638acbd5580fa0279fa270
skippybonez <108502188+skippybonez@users.noreply.github.com>
2024-01-21 21:12:04 -0500

    more work

--- src/AboutSection.js
+++ src/AboutSection.js
@@ -5,7 +5,8 @@ function AboutSection() {
     <Container>
       <h2>About Me</h2>
       <p>
-        Detailed information about your background, experience, or what you do.
+      The first two points I would make about myself are that I am passionate about cyber security and technology, and that I am detail-oriented with strong documentation skills. I feel that cybersecurity is one of the most important industries on earth right now, and I've got a bunch of personal experience dealing with cybersecurity.  From my girlfriend accidentally falling for phishing links and compromising her checking account to stumbling upon a massive data breach while casually scraping for customer data. My life experiences have taught me that confidentiality in the cyberspace is something which is overlooked by 90% of people. The next point made “Detail-oriented with strong documentation skills” is also something which is very important to me. One of my biggest grievances is miscommunication or not enough communication. Whenever I deliver my work or tool to another person, I always take my time to make sure every single detail is covered. It is an absolute shame when an entire process or experience is ruined over the smallest communication. 
+      This is why I feel asking questions is so valuable, as you truly can never be too sure. With all this being said I truly hope you can consider me as a candidate. Not only do my interests and values align directly with your role, but I guarantee that I will outwork the other candidates. I am so eager to learn with you. I appreciate the time you took to read my cover letter..
       </p>
     </Container>
   );
